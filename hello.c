2710ca45fb5d98784afd6bca5da5bbeb291ec836
Ranjith <rrr11072000@gmail.com>
2024-10-15 17:22:38 +0530

    C PROGRAMMING

---
+++ hello.c
@@ -0,0 +1,201 @@
+ /*#include<stdio.h>
+ int main()
+ {
+ 	printf("hello welcome !!!");
+ 	return 0;
+ }
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	printf("hello");
+ 	printf("welcome");
+ 	return 0;
+ }
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	printf("hello\n");
+ 	printf("welcome");
+ 	return 0;
+ }
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	printf("name\n");
+ 	printf("age\n");
+ 	printf("qualification\n");
+ 	printf("place");
+ 	return 0;
+ } 
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	printf("hai hello\n");
+ 	printf("good morning\n");
+ 	printf("thanks");
+ 	return 0;
+ } */
+ 
+ //printing numbers
+ 
+ /* #include<stdio.h>
+ int main()
+ {
+ 	printf("10\n");
+ 	printf("20\n");
+ 	printf("30\n");
+ 	printf("40\n");
+ 	printf("50\n");
+ 	printf("60\n");
+ 	printf("70\n");
+ 	return 0;
+ } */
+ 
+ /*//printing comment line
+ #include<stdio.h>
+ int main()
+ {
+ 	printf("hai hello\n");
+ 	printf("good morning\n");
+ 	//printf("to learn\n");
+ 	printf("c programming\n");
+ 	printf("thanks\n");
+ 	return 0;
+ } */
+ 
+ /*//printing integer datatype %d
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	int a;
+ 	a=100;
+ 	printf("%d",a);
+ 	return 0;
+ }
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	int a=100;
+ 	printf("%d",a);
+ 	return 0;
+ }*/
+ 
+/* //printing string datatype %s
+ 
+ #include<stdio.h>
+ int main()
+{
+	int a;
+	a='S';
+	printf("%c",a);
+	return 0;
+}
+ */
+ 
+/* //printing char datatype
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	char a[50]="systech";
+ 	printf("%s",a);
+ 	return 0;
+ } */
+ 
+/* //printing string and integer values
+ 
+ #include<stdio.h>
+ int main()
+ {
+ 	int x=10;
+ 	printf("%d\n",x);
+ 	char a[50]="systech";
+ 	printf("%s\n",a);
+ 	return 0;
+ }*/
+ 
+ /*//update value
+ #include<stdio.h>
+ int main()
+ {
+ 	int x;
+ 	x=10;
+ 	x=30;
+ 	printf("%d",x);
+ 	return 0;
+ }*/
+ 
+/* //printing character ASCII VALUE
+ #include<stdio.h>
+ int main()
+ {
+ 	char x='s';//ascii value=115
+ 	printf("%d",x);
+ 	return 0;
+ }*/
+ 
+ /*//printing values
+ #include<stdio.h>
+ int main()
+ {
+ 	int x;
+ 	x=20;
+ 	printf("%d\n",x);
+ 	x=30;
+ 	printf("%d",x);
+ 	return 0;
+         
+ }
+ 
+#include<stdio.h>
+int main()
+{
+	int x,a,b;
+	a=9;
+	b=10;
+	x=12;
+	printf("%d\n%d\n%d",x,a,b);
+	return 0;
+}
+
+//printing characters
+#include<stdio.h>
+int main()
+{
+	char x[50]="HII I am Ranjith";
+	printf("hello %s",x);
+	return 0;
+}*/
+
+/*//printing datatype sizes
+#include<stdio.h>
+int main()
+{
+	int a;
+	float v;
+	char t;
+	double b;
+	long int q;
+	int x=sizeof(a);
+	int y=sizeof(v);
+	int k=sizeof(t);
+	int h=sizeof(b);
+	int g=sizeof(q);
+	printf("%d\n",x);
+	printf("%d\n",y);
+	printf("%d\n",k);
+	printf("%d\n",h);
+	printf("%d\n",g);
+	return 0;
+	
+}*/
+
+
+ 
+  
